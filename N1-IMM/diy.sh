816bf37b4ee386be43e5bce1c493fa2de48fd3a2
Frank Zhang <rgb360@126.com>
2025-07-02 12:02:08 +0800

    Update diy.sh

--- N1-IMM/diy.sh
+++ N1-IMM/diy.sh
@@ -9,9 +9,8 @@ function git_sparse_clone() {
   cd .. && rm -rf $repodir
 }
 
-# Default IP
-sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
-
+# Default IP（192.168.1.1全局替换为192.168.10.2）
+sed -i 's/192.168.1.1/192.168.10.2/g' package/base-files/files/bin/config_generate
 
 # Add packages
 #添加科学上网源
@@ -22,7 +21,6 @@ git clone --depth 1 https://github.com/ophub/luci-app-amlogic package/amlogic
 git clone --depth 1 https://github.com/sbwml/luci-app-mosdns package/mosdns
 #git clone --depth 1 https://github.com/sbwml/luci-app-alist package/alist
 #git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall
-
 
 #删除库中的插件，使用自定义源中的包。
 rm -rf feeds/luci/themes/luci-theme-argon
